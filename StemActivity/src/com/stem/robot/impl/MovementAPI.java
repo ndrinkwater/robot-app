4fcdc7030bce19104823a8c0806e6e0cb8c1aa24
ndrinkwater <volatile_boy@yahoo.com>
2014-01-30 18:59:03 +0000

    Added stub areas for Robot API code

---
+++ StemActivity/src/com/stem/robot/impl/MovementAPI.java
@@ -0,0 +1,25 @@
+package com.stem.robot.impl;
+
+import com.stem.robot.interfaces.IMovementAPI;
+
+public class MovementAPI implements IMovementAPI {
+
+	@Override
+	public void extendArm() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void retractArm() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void turnAxis(double angle) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ StemActivity/src/com/stem/robot/interfaces/IMovementAPI.java
@@ -0,0 +1,8 @@
+package com.stem.robot.interfaces;
+
+public interface IMovementAPI {
+	
+	public void extendArm();
+	public void retractArm();
+	public void turnAxis(double angle);
+}
